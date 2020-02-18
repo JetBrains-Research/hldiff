5d17b0e7371deace3e10a92831e3f8ac9d91d45f
Dmitriy Abramov <karvozavr@gmail.com>
2020-02-18 14:48:45 +0300

    Update build.gradle.kts

--- hldiff-core/build.gradle.kts
+++ hldiff-core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     testCompile("junit", "junit", "4.12")
     compile(fileTree("include" to listOf("*.jar"), "dir" to "libs"))
     compile(project(":python-gen"))
+    compile("com.xenomachina:kotlin-argparser:2.0.7")
 }
 
 configure<JavaPluginConvention> {
