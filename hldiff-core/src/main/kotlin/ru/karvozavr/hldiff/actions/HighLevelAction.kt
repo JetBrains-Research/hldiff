3782be47aa856a8bf5c6db7d3f17a0ba28690002
Dmitriy Abramov <karvozavr@gmail.com>
2020-02-18 17:20:01 +0300

    Add prettyPrint

--- hldiff-core/src/main/kotlin/ru/karvozavr/hldiff/actions/HighLevelAction.kt
+++ hldiff-core/src/main/kotlin/ru/karvozavr/hldiff/actions/HighLevelAction.kt
@@ -2,6 +2,7 @@ package ru.karvozavr.hldiff.actions
 
 import com.github.gumtreediff.actions.model.*
 import com.github.gumtreediff.tree.ITree
+import com.github.gumtreediff.tree.TreeContext
 import ru.karvozavr.hldiff.language.LanguageInfo
 
 abstract class HighLevelAction(val node: ITree) {
@@ -12,14 +13,24 @@ abstract class HighLevelAction(val node: ITree) {
             val node = lowLevelAction.node
 
             val nodeTypeString = languageInfo.getTypeName(node)
-            val actionObjectType = if (isPartial) nodeTypeString + "Part" else nodeTypeString
+            val actionObjectType = if (isPartial) "$nodeTypeString Partial" else nodeTypeString
 
             return when (lowLevelAction) {
                 is Delete -> DeleteAction(node, actionObjectType)
                 is Update -> TODO()
-                is Move -> MoveAction(node, actionObjectType, lowLevelAction.parent, lowLevelAction.position)
-                is Insert -> InsertAction(node, actionObjectType, lowLevelAction.parent, lowLevelAction.position)
+                is Move -> MoveAction(node, actionObjectType, languageInfo.getTypeName(lowLevelAction.parent), lowLevelAction.position)
+                is Insert -> InsertAction(node, actionObjectType, languageInfo.getTypeName(lowLevelAction.parent), lowLevelAction.position)
                 else -> throw RuntimeException("Low-level action of unknown type - ${lowLevelAction.javaClass}.")
+            }
+        }
+
+        fun prettyPrint(action: HighLevelAction, before: String, after: String): String {
+            return when (action) {
+                is InsertAction -> action.toString()
+                is DeleteAction -> action.toString()
+                is UpdateAction -> before.substring(action.node.pos, action.node.pos + 15) + "...    " + action.toString()
+                is MoveAction -> action.toString()
+                else -> throw RuntimeException("Unknown high-level action type")
             }
         }
     }
--- hldiff-core/src/main/kotlin/ru/karvozavr/hldiff/actions/MoveAction.kt
+++ hldiff-core/src/main/kotlin/ru/karvozavr/hldiff/actions/MoveAction.kt
@@ -2,9 +2,9 @@ package ru.karvozavr.hldiff.actions
 
 import com.github.gumtreediff.tree.ITree
 
-class MoveAction(node: ITree, val type: String, val newParent: ITree, val position: Int) : HighLevelAction(node) {
+class MoveAction(node: ITree, val type: String, val newParent: String, val position: Int) : HighLevelAction(node) {
 
     override fun toString(): String {
-        return "Move $type to ${newParent.toShortString()} at position $position"
+        return "Move $type to $newParent at position $position"
     }
 }
