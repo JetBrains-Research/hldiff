061996a82b129d385c73d2d8f2d8c837558f9772
karvozavr <karvozavr@gmail.com>
2020-03-31 14:10:41 +0300

    Modify program arguments

--- hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/HLDiffArgs.kt
+++ hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/HLDiffArgs.kt
@@ -1,10 +1,32 @@
 package ru.karvozavr.hldiff.app
 
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class HLDiffArgs(parser: ArgParser) {
 
-    val source by parser.positional("SOURCE", help = "source code (before changes) filename")
-    val destination by parser.positional("DEST", help = "destination code (after changes) filename")
-    val visualize by parser.flagging("--html", help = "output html visualisation")
+    val source by parser.positional("SOURCE", help = "source code (before changes) filename").default("")
+
+    val destination by parser.positional("DEST", help = "destination code (after changes) filename").default("")
+
+    val visualizeHtml by parser.flagging("--html", help = "output html visualisation")
+
+    val batch by parser.flagging(
+            "--batch",
+            help = "Process multiple file pairs. File name pairs should be passed to stdin, one pair per line like this: \"fileBefore.java fileAfter.java>\".")
+
+    val outputDirectory: Path by parser.storing(
+            "-o", "--outdir",
+            help = "Output directory for batch processing.")
+    {
+        val dir = Paths.get(this)
+        if (!Files.exists(dir)) {
+            Files.createDirectories(dir)
+        } else {
+            dir
+        }
+    }.default(Files.createDirectories(Paths.get("hldiff-output")))
 }
