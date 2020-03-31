837d1167ba72bf1df3350b823260952ca6606c49
karvozavr <karvozavr@gmail.com>
2020-03-31 14:11:29 +0300

    Extend CLI with batch processing functionality

--- hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/HLDiffCLI.kt
+++ hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/HLDiffCLI.kt
@@ -5,25 +5,75 @@ import ru.karvozavr.hldiff.data.HLDiffFormatter
 import ru.karvozavr.hldiff.data.HighLevelDiff
 import ru.karvozavr.hldiff.visualisation.VisualisationGenerator
 import java.io.File
-import java.nio.file.Paths
+import java.lang.Exception
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
 class HLDiffCLI(private val args: HLDiffArgs) {
 
-    val api: HLDiffAPI = HLDiffAPI()
+    private val api: HLDiffAPI = HLDiffAPI()
+    //../hldiff-core/src/test/resources/a.py
+    fun runHLDiff() {
+        if (args.batch) {
+            val outputDir = args.outputDirectory
+            val filePairs = mutableListOf<FilePair>()
+            var line: String? = readLine()
+            var lineNumber = -1
+            while (line != null) {
+                ++lineNumber
 
-    fun runHLDiffForFilePair() {
-        val src = Paths.get(args.source).toAbsolutePath().toString()
-        val dst = Paths.get(args.destination).toAbsolutePath().toString()
+                val split = line.trim().split(Regex("\\s+"))
+                if (split.size != 2) {
+                    System.err.println("Incorrect input line $lineNumber:\n$line")
+                    line = readLine()
+                    continue
+                }
+
+                filePairs.add(FilePair(split[0], split[1]))
+                line = readLine()
+            }
+
+            runHLDiffForFilePairs(filePairs, outputDir)
+        } else {
+            runHLDiffForFilePair()
+        }
+    }
+
+    private fun runHLDiffForFilePairs(pairs: List<FilePair>, outputDir: Path) {
+        // TODO make it parallel
+        for (pair in pairs) {
+            try {
+                val diff = api.getHighLevelDiff(pair.fileBefore, pair.fileAfter)
+                val output = formatDiff(pair.fileBefore, pair.fileAfter, diff)
+                val outFilePath = outputDir.resolve(pair.diffName + (if (args.visualizeHtml) ".html" else ".json"))
+                outFilePath.toFile().writeText(output)
+            } catch (e: Exception) {
+                System.err.println("Failed to generate diff for file pair: ${pair.fileBefore} ${pair.fileAfter}")
+                e.printStackTrace(System.err)
+            }
+        }
+    }
+
+    private fun runHLDiffForFilePair() {
+        val src = args.source
+        val dst = args.destination
         val result: HighLevelDiff = api.getHighLevelDiff(src, dst)
 
+        val output = formatDiff(src, dst, result)
+
+        println(output)
+    }
+
+    private fun formatDiff(src: String, dst: String, result: HighLevelDiff): String {
         val codeBefore = File(src).readText()
         val codeAfter = File(dst).readText()
         val formatter = HLDiffFormatter(result, codeBefore, codeAfter)
 
-        if (args.visualize) {
-            println(VisualisationGenerator().generate(formatter.formatJSON(), codeBefore, codeAfter))
+        return if (args.visualizeHtml) {
+            VisualisationGenerator().generate(formatter.formatJSON(), codeBefore, codeAfter)
         } else {
-            println(formatter.formatJSON())
+            formatter.formatJSON()
         }
     }
 }
