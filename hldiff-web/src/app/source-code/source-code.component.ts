b66fbac6e5b86e6c9bc8fa0337917699cf0a5363
karvozavr <karvozavr@gmail.com>
2020-04-06 14:55:48 +0300

    Add source-code component

---
+++ hldiff-web/src/app/source-code/source-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-source-code',
+  templateUrl: './source-code.component.html',
+  styleUrls: ['./source-code.component.scss']
+})
+export class SourceCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
