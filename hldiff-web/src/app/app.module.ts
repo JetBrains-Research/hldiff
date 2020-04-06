f5393b996a553872f7956fea788f8ccc0159eed4
karvozavr <karvozavr@gmail.com>
2020-04-06 12:11:58 +0300

    Init diff component

--- hldiff-web/src/app/app.module.ts
+++ hldiff-web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiffComponent } from './diff/diff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiffComponent
   ],
   imports: [
     BrowserModule,
