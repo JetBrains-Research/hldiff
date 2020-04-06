ad5fd5fe2b14341ce3b80ca79fe120e53bcf4947
karvozavr <karvozavr@gmail.com>
2020-04-06 14:01:21 +0300

    Setup routing for diff component

--- hldiff-web/src/app/app-routing.module.ts
+++ hldiff-web/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiffComponent } from "./diff/diff.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/diff', pathMatch: 'full' },
+  { path: 'diff', component: DiffComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
