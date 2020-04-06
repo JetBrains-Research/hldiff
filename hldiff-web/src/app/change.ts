cb1505ac04831bd66ac040ea2c7d8ae36d8b1eb2
karvozavr <karvozavr@gmail.com>
2020-04-06 16:41:17 +0300

    Fix interface of change.ts

--- hldiff-web/src/app/change.ts
+++ hldiff-web/src/app/change.ts
@@ -1,11 +1,11 @@
 export interface Change {
-  id: 0,
-  nodeId: 58,
-  type: "move",
-  name: "Move MethodInvocation to VariableDeclarationFragment at position 1",
-  groupedActions: [],
-  startPosition: 289,
-  endPosition: 321,
-  startPositionAfter: 298,
-  endPositionAfter: 330
+  id: number,
+  nodeId: number,
+  type: string,
+  name: string,
+  groupedActions: Array<any>,
+  startPosition: number,
+  endPosition: number,
+  startPositionAfter: number,
+  endPositionAfter: number
 }
