59997ae5cc63ee18dd9ec547a50d922e901812b6
karvozavr <karvozavr@gmail.com>
2020-04-06 21:38:07 +0300

    Extract change-action component

--- hldiff-web/src/app/change-action/change-action.component.ts
+++ hldiff-web/src/app/change-action/change-action.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Change } from '../change';
+import { drawLine, setAlpha } from '../draw-utils';
 
 @Component({
   selector: 'app-change-action',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeActionComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  change: Change;
+
+  @ViewChild('rootChange')
+  private element: ElementRef;
+
+  constructor(element: ElementRef) {
+    this.element = element;
+  }
 
   ngOnInit(): void {
   }
 
+  enter(): void {
+    const line1 = document.getElementById('line');
+    const line2 = document.getElementById('line2');
+    const codeChanges = document.querySelectorAll<HTMLElement>('.code-change-' + this.change.id);
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < codeChanges.length; i++) {
+      const change = codeChanges[i];
+      change.style.backgroundColor = setAlpha(document.defaultView.getComputedStyle(change, null)['background-color'], 1);
+    }
+
+    switch (this.change.type) {
+      case 'add':
+        drawLine(codeChanges[0], this.element.nativeElement, line1, 'var(--add-color)');
+        break;
+
+      case 'move':
+        drawLine(codeChanges[0], this.element.nativeElement, line1, 'var(--move-color)');
+        drawLine(codeChanges[1], this.element.nativeElement, line2, 'var(--move-color)');
+        break;
+
+      case 'delete':
+        drawLine(codeChanges[0], this.element.nativeElement, line1, 'var(--delete-color)');
+        break;
+
+      case 'update':
+        drawLine(codeChanges[0], this.element.nativeElement, line1, 'var(--update-color)');
+        drawLine(codeChanges[1], this.element.nativeElement, line2, 'var(--update-color)');
+        break;
+    }
+  }
+
+  leave(): void {
+    const line1 = document.getElementById('line');
+    const line2 = document.getElementById('line2');
+    const codeChanges = document.querySelectorAll<HTMLElement>('.code-change-' + this.change.id);
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < codeChanges.length; i++) {
+      const change = codeChanges[i];
+      line1.setAttribute('x1', '0');
+      line1.setAttribute('y1', '0');
+      line1.setAttribute('x2', '0');
+      line1.setAttribute('y2', '0');
+      line2.setAttribute('x1', '0');
+      line2.setAttribute('y1', '0');
+      line2.setAttribute('x2', '0');
+      line2.setAttribute('y2', '0');
+      change.style.backgroundColor = setAlpha(document.defaultView.getComputedStyle(change, null)['background-color'], 0.3);
+    }
+  }
 }
--- hldiff-web/src/app/diff/diff.component.ts
+++ hldiff-web/src/app/diff/diff.component.ts
@@ -34,26 +34,6 @@ export class DiffComponent implements OnInit, AfterViewInit {
 
     this.positionChanges(changesList);
     this.setChangedCodeListeners(changedCode, line);
-    this.setChangesListListeners(changesList, line, line2);
-  }
-
-  drawLine(start, end, line, color = 'rgb(0, 0, 0)') {
-    let x1 = start.getBoundingClientRect().left;
-    const y1 = start.offsetTop + start.getBoundingClientRect().height / 2;
-    let x2 = end.getBoundingClientRect().left;
-    const y2 = end.getBoundingClientRect().top + window.scrollY + end.clientHeight / 2;
-
-    if (x1 < x2) { // code is to the left
-      x1 += start.getBoundingClientRect().width;
-    } else {
-      x2 += end.clientWidth;
-    }
-
-    document.documentElement.style.setProperty('--line-color', this.setAlpha(color, 1));
-    line.setAttribute('x1', x1);
-    line.setAttribute('y1', y1);
-    line.setAttribute('x2', x2);
-    line.setAttribute('y2', y2);
   }
 
   setChangedCodeListeners(changedCode, line) {
@@ -65,56 +45,6 @@ export class DiffComponent implements OnInit, AfterViewInit {
         const end = document.getElementById(changeId);
 
         // this.drawLine(start, end, line);
-      });
-    }
-  }
-
-  setAlpha(rgba, alpha) {
-    if (rgba.startsWith('rgba')) {
-      return rgba.replace(/ ([^ ]*)\)/, alpha);
-    } else {
-      return rgba.replace('rgb', 'rgba').replace(/(\))/, ', ' + alpha + ')');
-    }
-  }
-
-  setChangesListListeners(changesList, line, line2) {
-    for (const elem of changesList) {
-      const codeChanges = document.querySelectorAll<HTMLElement>('.' + elem.id);
-
-      elem.addEventListener('mouseenter', () => {
-        // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < codeChanges.length; i++) {
-          const change = codeChanges[i];
-          change.style.backgroundColor = this.setAlpha(document.defaultView.getComputedStyle(change, null)['background-color'], 1);
-        }
-
-        if (elem.className.includes('change-add')) {
-          this.drawLine(codeChanges[0], elem, line, 'var(--add-color)');
-        } else if (elem.className.includes('change-move')) {
-          this.drawLine(codeChanges[0], elem, line, 'var(--move-color)');
-          this.drawLine(codeChanges[1], elem, line2, 'var(--move-color)');
-        } else if (elem.className.includes('change-delete')) {
-          this.drawLine(codeChanges[0], elem, line, 'var(--delete-color)');
-        } else if (elem.className.includes('change-update')) {
-          this.drawLine(codeChanges[0], elem, line, 'var(--update-color)');
-          this.drawLine(codeChanges[1], elem, line2, 'var(--update-color)');
-        }
-      });
-
-      elem.addEventListener('mouseout', () => {
-        // tslint:disable-next-line:prefer-for-of
-        for (let i = 0; i < codeChanges.length; i++) {
-          const change = codeChanges[i];
-          line.setAttribute('x1', 0);
-          line.setAttribute('y1', 0);
-          line.setAttribute('x2', 0);
-          line.setAttribute('y2', 0);
-          line2.setAttribute('x1', 0);
-          line2.setAttribute('y1', 0);
-          line2.setAttribute('x2', 0);
-          line2.setAttribute('y2', 0);
-          change.style.backgroundColor = this.setAlpha(document.defaultView.getComputedStyle(change, null)['background-color'], 0.3);
-        }
       });
     }
   }
