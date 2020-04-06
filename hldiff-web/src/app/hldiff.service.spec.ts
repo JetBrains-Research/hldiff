83767de4124c2317d64444d4405a896ac1fba131
karvozavr <karvozavr@gmail.com>
2020-04-06 16:40:47 +0300

    Implement stub hldiff-service

--- hldiff-web/src/app/hldiff.service.spec.ts
+++ hldiff-web/src/app/hldiff.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HldiffService } from './hldiff.service';
+import { HLDiffService } from './hldiff.service';
 
 describe('HldiffService', () => {
-  let service: HldiffService;
+  let service: HLDiffService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(HldiffService);
+    service = TestBed.inject(HLDiffService);
   });
 
   it('should be created', () => {
