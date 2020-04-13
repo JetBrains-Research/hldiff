e60498aac897dff6592c2c5e0c85df6b4f600bda
karvozavr <karvozavr@gmail.com>
2020-04-13 19:44:02 +0300

    Implement auth service for basic authentication

--- hldiff-web/src/app/users/authentication.service.ts
+++ hldiff-web/src/app/users/authentication.service.ts
@@ -1,28 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserDto } from './user-dto';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() {
+  private USER_KEY = 'currentUser';
+
+  constructor(private http: HttpClient) {
   }
 
-  authenticate(username: string, password: string): boolean {
-    if (username === 'karvozavr' && password === '123') {
-      sessionStorage.setItem('username', username);
-      return true;
-    } else {
-      return false;
-    }
+  authenticate(username: string, password: string): Observable<UserDto> {
+    return this.http.post<UserDto>(`${environment.apiUrl}/user/authenticate`, { username, password } as UserDto).pipe(
+      tap(user => {
+        if (user) {
+          user.authdata = window.btoa(user.username + ':' + user.password);
+          sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
+        }
+      })
+    );
   }
 
   isUserLoggedIn(): boolean {
-    return sessionStorage.getItem('username') !== null;
+    return sessionStorage.getItem(this.USER_KEY) !== null;
   }
 
-  getUserName(): string {
-    return sessionStorage.getItem('username');
+  getUser(): User {
+    return JSON.parse(sessionStorage.getItem(this.USER_KEY)) as UserDto;
   }
 
   logout() {
---
+++ hldiff-web/src/app/users/user-dto.ts
@@ -0,0 +1,5 @@
+export interface UserDto {
+  username: string;
+  password: string;
+  authdata: string;
+}
