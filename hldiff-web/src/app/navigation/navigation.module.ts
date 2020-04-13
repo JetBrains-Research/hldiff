07aa13c8fd07711b6e10f6bea77ff5b0797e6a06
karvozavr <karvozavr@gmail.com>
2020-04-13 14:32:38 +0300

    Add buttons to navbar and style it

--- hldiff-web/src/app/navigation/navigation.module.ts
+++ hldiff-web/src/app/navigation/navigation.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { MaterialModule } from '../material/material.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
   declarations: [NavigationBarComponent],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   exports: [
     NavigationBarComponent,
