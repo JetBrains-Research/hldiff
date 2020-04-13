3c5a56b358edc0a428ad07171d4e2f9d347f597a
karvozavr <karvozavr@gmail.com>
2020-04-13 14:56:31 +0300

    Add Authentication service

--- hldiff-web/src/app/navigation/navigation-bar/navigation-bar.component.ts
+++ hldiff-web/src/app/navigation/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../authentication.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
