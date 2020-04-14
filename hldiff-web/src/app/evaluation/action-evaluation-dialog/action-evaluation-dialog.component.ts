ed973daf080d776fa79168f6985f7d5b55591a2d
karvozavr <karvozavr@gmail.com>
2020-04-14 18:07:22 +0300

    Fix dialog form implementation - remove formControl

--- hldiff-web/src/app/evaluation/action-evaluation-dialog/action-evaluation-dialog.component.ts
+++ hldiff-web/src/app/evaluation/action-evaluation-dialog/action-evaluation-dialog.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Change } from '../../change';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { EvaluationService } from '../evaluation.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../../users/login-dialog/login-dialog.component';
+
+export interface EvalDialogData {
+  qualityValue: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-action-evaluation-dialog',
@@ -15,23 +20,9 @@ export class ActionEvaluationDialogComponent implements OnInit {
   @Input()
   change: Change;
 
-  error: string;
-
-  form: FormGroup = new FormGroup({
-    comment: new FormControl(''),
-    radio: new FormControl('', [Validators.required]),
-  });
-
-  selectedModel: string;
-
-  constructor(private evaluationService: EvaluationService,
-              private dialogRef: MatDialogRef<LoginDialogComponent>) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: EvalDialogData) {
   }
 
   ngOnInit(): void {
-  }
-
-  submit() {
-    this.dialogRef.close();
   }
 }
