4991909dacb3d8fb83a54dd75d2d9e19ea4f8c2d
karvozavr <karvozavr@gmail.com>
2020-04-24 15:25:11 +0300

    Add diffName as param

--- hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/FilePair.kt
+++ hldiff-cli/src/main/kotlin/ru/karvozavr/hldiff/app/FilePair.kt
@@ -2,7 +2,8 @@ package ru.karvozavr.hldiff.app
 
 import java.nio.file.Paths
 
-data class FilePair(val fileBefore: String, val fileAfter: String) {
-
-    val diffName = "${Paths.get(fileBefore).fileName}__${Paths.get(fileAfter).fileName}"
-}
+data class FilePair(
+  val fileBefore: String,
+  val fileAfter: String,
+  val diffName: String = "${Paths.get(fileBefore).fileName}__${Paths.get(fileAfter).fileName}"
+)
