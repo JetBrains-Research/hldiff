8e2fa3f2e6bc369a09a11574f005c08c6363c1c5
Dmitrii Abramov <karvozavr@gmail.com>
2020-05-04 20:08:15 +0300

    Fix style

--- hldiff-web/src/app/change.ts
+++ hldiff-web/src/app/change.ts
@@ -1,11 +1,11 @@
 export interface Change {
-  id: number,
-  nodeId: number,
-  type: string,
-  name: string,
-  groupedActions: Array<any>,
-  startPosition: number,
-  endPosition: number,
-  startPositionAfter: number,
-  endPositionAfter: number
+  id: number;
+  nodeId: number;
+  type: string;
+  name: string;
+  groupedActions: Array<any>;
+  startPosition: number;
+  endPosition: number;
+  startPositionAfter: number;
+  endPositionAfter: number;
 }
